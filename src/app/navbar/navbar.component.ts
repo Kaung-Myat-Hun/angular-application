53cf1f8b2052601e466297aeed5b17cd5baac6c5
kaung <kaungmyathun7@gmail.com>
2023-05-15 14:05:17 +0630

    adding: angular class binding

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  fix = false;
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    document.addEventListener('scroll', () => {
+      window.pageYOffset >= 65 ? (this.fix = true) : (this.fix = false);
+    });
+  }
 }
