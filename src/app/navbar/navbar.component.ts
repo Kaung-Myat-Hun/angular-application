02bf6daeaed1f7f30c24dd1a8565449f87f7457a
kaung <kaungmyathun7@gmail.com>
2023-05-18 09:20:52 +0630

    updating: navbar hide and show at login page and main page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,14 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  fix = false;
-
+  fix: boolean = false;
+  hide: boolean = false;
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     document.addEventListener('scroll', () => {
       window.pageYOffset >= 65 ? (this.fix = true) : (this.fix = false);
     });
+    let location = window.location.pathname;
+    // console.log(location, 'current-url');
+    if (location === '/') {
+      this.hide = true;
+    } else {
+      this.hide = false;
+    }
   }
 
   logoutHandler() {
